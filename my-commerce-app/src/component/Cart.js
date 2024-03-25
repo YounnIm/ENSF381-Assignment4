b7924e830522e6dd38033cf44647b87b7b07a597
Taiwu <chen.taiwu@ucalgary.ca>
2024-03-25 12:13:47 -0600

    update style

--- my-commerce-app/src/component/Cart.js
+++ my-commerce-app/src/component/Cart.js
@@ -5,11 +5,12 @@ function Cart({ cartItems, removeFromCart }) {
   const totalCost = cartItems.reduce((sum, item) => sum + item.quantity * item.product.price, 0);
 
   return (
-    <div>
-      {cartItems.map(item => (
-        <CartItem key={item.product.id} item={item} removeFromCart={removeFromCart} />
-      ))}
-      <p>Total Cost: ${totalCost.toFixed(2)}</p>
+    <div style={{ marginLeft: '1000px' }}>
+    <h2>Shopping Cart</h2>
+    {cartItems.map(item => (
+      <CartItem key={item.product.id} item={item} removeFromCart={removeFromCart} />
+    ))}
+    <p>Total Cost: ${totalCost.toFixed(2)}</p>
     </div>
   );
 }
--- my-commerce-app/src/component/CartItem.js
+++ my-commerce-app/src/component/CartItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function CartItem({ item, removeFromCart }) {
   return (
     <div>
-      <img src={item.product.image} alt={item.product.name} />
+      <img style={{ width: '200px', height: 'auto' }} src={item.product.image} alt={item.product.name} />
       <h2>{item.product.name}</h2>
       <p>${item.product.price.toFixed(2)}</p>
       <p>Quantity: {item.quantity}</p>
