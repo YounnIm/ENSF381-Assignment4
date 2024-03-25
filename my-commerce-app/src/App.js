ae0a21e66b570bd83e5f9073da214b1258e7a38f
Taiwu <chen.taiwu@ucalgary.ca>
2024-03-25 11:54:05 -0600

    test for product page

--- my-commerce-app/src/App.js
+++ my-commerce-app/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import Homepage from './component/Homepage';
 import ProductPage from './component/ProductPage';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-    <Homepage />
     <ProductPage />
     </div>
   );
-};
+}
 
 export default App;
 
 
+  
   
